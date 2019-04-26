94ef5cf64bfcc92cea6bde9772a892790880f5bf
Tobias Hoppenthaler <tobias.hoppenthaler@gmail.com>
2019-04-26 12:46:18 +0200

    added all abis to build so build will not fail

--- DotnetCologne/DotnetCologne/MainPage.xaml.cs
+++ DotnetCologne/DotnetCologne/MainPage.xaml.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
 using Xamarin.Forms;
 
 namespace DotnetCologne
@@ -16,22 +18,29 @@ namespace DotnetCologne
 
         private void OnHoldMeButtonClicked(object sender, EventArgs e)
         {
-            throw new NotImplementedException();
+            Analytics.TrackEvent("Hold Me clicked");
         }
 
         private void OnThrillMeButtonClicked(object sender, EventArgs e)
         {
-            throw new NotImplementedException();
+            Analytics.TrackEvent("Thrill me clicked");
+            try
+            {
+                int y = (int) sender;
+            } catch (Exception exception) {
+                Crashes.TrackError(exception);
+            }
         }
 
         private void OnKissMeButtonClicked(object sender, EventArgs e)
         {
-            throw new NotImplementedException();
+            Analytics.TrackEvent("Kiss Me clicked");
         }
 
         private void OnKillMeButtonClicked(object sender, EventArgs e)
         {
-            throw new NotImplementedException();
+            Analytics.TrackEvent("Kill me clicked");
+            Crashes.GenerateTestCrash();
         }
     }
 }
